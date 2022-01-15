6910ca23fb8a8d67ef52f1f21d1192f9afc5f4a8
RoundABoy <83515518+RoundABoy@users.noreply.github.com>
2022-01-15 13:55:59 +0000

    Add files via upload

---
+++ voting.py
@@ -0,0 +1,6 @@
+print("Are you 18 or above years old?")
+ch=int(input("Enter 1 for yes \nEnter 2 for no"))
+if(ch==1):
+ print("You can vote!!")
+elif(ch==2):
+ print("You cannot vote, wait a few years!")
